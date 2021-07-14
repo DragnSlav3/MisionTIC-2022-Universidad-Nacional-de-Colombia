a481d2894cb9a67150a1cc90c5f50c5581c44c4b
DragnSlav3 <dragnslav3@gmail.com>
2021-07-13 20:43:24 -0500

    primer commit (ya puedo ver y consultar los datos a discresión)

---
+++ Ciclo 1/Otros códigos/Propios/covid_owid/covid_owid.py
@@ -0,0 +1,36 @@
+import json
+import requests
+import csv
+from pprint import pprint
+
+csv_data = requests.get("https://covid.ourworldindata.org/data/owid-covid-data.csv")
+
+json_data = requests.get("https://covid.ourworldindata.org/data/owid-covid-data.json")
+
+json1 = json.loads(json_data.text)
+
+claves = []
+
+for k, v in json1.items():
+  if k not in claves and k.startswith("OWID") == False:
+    claves.append(k)
+
+pprint(claves)
+print()
+print(len(claves))
+print()
+pprint(json1["COL"]["data"][-1])
+print(len(json1["COL"]["data"][-1]))
+print(type((json1["COL"]["data"][-1])))
+
+
+
+claves1 = []
+for k, v in json1["COL"].items():
+  if k not in claves1:
+    claves1.append(k)
+
+pprint(claves1)
+
+for pais in claves:
+  print(json1[pais]['location'], " -> ", int(json1[pais]['population']))
