ecbd667dbf817cfd107bcac2d5cc7b21b6afd4b9
DragnSlav3 <dragnslav3@gmail.com>
2021-07-13 22:24:54 -0500

    Identificados los países sin reportes y exceptuados de la salida de datos

--- Ciclo 1/Otros códigos/Propios/covid_owid/covid_owid.py
+++ Ciclo 1/Otros códigos/Propios/covid_owid/covid_owid.py
@@ -9,17 +9,27 @@ json_data = requests.get("https://covid.ourworldindata.org/data/owid-covid-data.
 
 json1 = json.loads(json_data.text)
 
-claves = []
+paises = []
+sinreportes = []
 
 for k, v in json1.items():
-  if k not in claves and k.startswith("OWID") == False:
-    claves.append(k)
+    if k not in paises and k.startswith("OWID") == False:
+        paises.append(k)
+    if 'total_deaths' not in v["data"][-1]:
+        sinreportes.append(k)
 
-pprint(claves)
+conreportes = []
+for pais in paises:
+    if pais not in sinreportes:
+        conreportes.append(pais)
+
+pprint(paises)
+print(sinreportes)
 print()
-print(len(claves))
+print(len(paises))
 print()
-data = json1["AIA"]["data"][-1]
+
+data = json1["ISR"]["data"][-1]
 print("***")
 #print(int(json1["AIA"]["data"][-1]['total_deaths']))
 print("***")
@@ -35,7 +45,14 @@ for k, v in json1["COL"].items():
 
 pprint(claves1)
 
-for pais in claves:
+for pais in conreportes:
     print(f"\nPaís: {json1[pais]['location']} ({pais})")
     print("Población: ", int(json1[pais]['population']))
     print("Total muertes COVID: ", (int(json1[pais]["data"][-1]['total_deaths'])))
+    print("Muertes por millón de hab.: ", int((int(json1[pais]["data"][-1]['total_deaths']))/(json1[pais]['population']/1000000)))
+    print("Dato oficial: ", int(json1[pais]["data"][-1]['total_deaths_per_million']))
+    print(json1[pais]["data"][-1]['date'])
+
+# PENDIENTE PROBAR TRY EXCEPT CON LOS PAÍSES SIN REPORTES
+# PENDIENTE GENERAR JSON A PARTIR DE ESTA INFORMACIÓN
+# PENDIENTE GENERAR ALGÚN TIPO DE VISUALIZACIÓN
