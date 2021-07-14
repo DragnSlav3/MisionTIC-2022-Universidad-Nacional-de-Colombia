9f7b0dba02a6f3ebbaca622f810ce25bb16e2d29
DragnSlav3 <dragnslav3@gmail.com>
2021-07-14 12:04:53 -0500

    Captura y limpieza de datos finalizada, se almacena en jsonsalida.json

--- Ciclo 1/Otros códigos/Propios/covid_owid/covid_owid.py
+++ Ciclo 1/Otros códigos/Propios/covid_owid/covid_owid.py
@@ -1,17 +1,25 @@
+# Este script descarga la última actualización sobre información COVID de la página 
+# Our World In Data, disponible en GitHub, en formato JSON.
+# Crea una lista con la abreviatura de cada país, excluyendo las entradas con encabezado 
+# "OWID", que no corresponden a países. Algunos países no cuentan con la información sobre 
+# el número total de muertes, estos son almacenados en la variable "sinreportes", y se
+# excluyen del proceso. 
+# La información sobre el total de casos y el total de muertes en la base de datos original
+# se encuentra dentro de un diccionario llamado "data" por cada país, por lo que el script
+# accede únicamente a la entrada más reciente [-1]. La fecha se tiene en cuenta y se almacena.
+# Aunque el archivo original también incluye el dato de muertes por millón de habitantes,
+# este dato se genera de manera dinámica haciendo la división correspondiente.
+# La información de cada país se almacena en formato diccionario y se agreag a la variable lista
+# "jsonsalida", la cual es convertida y guardada en un archivo JSON llamado "jsonsalida.json".
+
 import json
 import requests
-import csv
-from pprint import pprint
-
-csv_data = requests.get("https://covid.ourworldindata.org/data/owid-covid-data.csv")
 
 json_data = requests.get("https://covid.ourworldindata.org/data/owid-covid-data.json")
-
 json1 = json.loads(json_data.text)
 
 paises = []
 sinreportes = []
-
 for k, v in json1.items():
     if k not in paises and k.startswith("OWID") == False:
         paises.append(k)
@@ -23,36 +31,18 @@ for pais in paises:
     if pais not in sinreportes:
         conreportes.append(pais)
 
-pprint(paises)
-print(sinreportes)
-print()
-print(len(paises))
-print()
-
-data = json1["ISR"]["data"][-1]
-print("***")
-#print(int(json1["AIA"]["data"][-1]['total_deaths']))
-print("***")
-pprint(data)
-print(len(data))
-print(type(data))
-
-
-claves1 = []
-for k, v in json1["COL"].items():
-  if k not in claves1:
-    claves1.append(k)
-
-pprint(claves1)
-
+jsonsalida = []
 for pais in conreportes:
-    print(f"\nPaís: {json1[pais]['location']} ({pais})")
-    print("Población: ", int(json1[pais]['population']))
-    print("Total muertes COVID: ", (int(json1[pais]["data"][-1]['total_deaths'])))
-    print("Muertes por millón de hab.: ", int((int(json1[pais]["data"][-1]['total_deaths']))/(json1[pais]['population']/1000000)))
-    print("Dato oficial: ", int(json1[pais]["data"][-1]['total_deaths_per_million']))
-    print(json1[pais]["data"][-1]['date'])
+    diccionario = {
+        "abreviacion":pais, 
+        "nombre":json1[pais]['location'],
+        "poblacion": int(json1[pais]['population']), 
+        "total_muertes":int(json1[pais]["data"][-1]['total_deaths']),
+        "muertes_x_millon":int((int(json1[pais]["data"][-1]['total_deaths']))/(json1[pais]['population']/1000000)), 
+        "fecha":json1[pais]["data"][-1]['date']
+        }
+    jsonsalida.append(diccionario)
 
-# PENDIENTE PROBAR TRY EXCEPT CON LOS PAÍSES SIN REPORTES
-# PENDIENTE GENERAR JSON A PARTIR DE ESTA INFORMACIÓN
-# PENDIENTE GENERAR ALGÚN TIPO DE VISUALIZACIÓN
+with open("jsonsalida.json", "w") as datossalida:
+    json.dump(jsonsalida, datossalida, indent=4)
+print(f"Archivo JSON generado satisfactoriamente. Total de entradas: {len(jsonsalida)}.")
